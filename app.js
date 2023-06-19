fd2d7b47bdb9807796a6fc5c985c812ea6d1453d
DylanPiriou <dylan.piriou@hotmail.com>
2023-06-19 16:18:39 +0200

    progress bar added

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const stickyElements = [...document.querySelectorAll(".sticky")];
 console.log(stickyElements)
 
 window.addEventListener("scroll", () => {
-    for(let i = 0; i < stickyElements.length; i++){
+    for (let i = 0; i < stickyElements.length; i++) {
         transform(stickyElements[i])
     }
 
-    function transform(section){
+    function transform(section) {
         const offsetTop = section.parentElement.offsetTop;
         const scrollSection = section.querySelector(".scroll-section");
         console.log(scrollSection)
@@ -14,4 +14,16 @@ window.addEventListener("scroll", () => {
         // percentage = percentage < 0 ? 0 : percentage > 400 ? 400 : percentage;
         scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`;
     }
+})
+
+
+// PROGRESS BAR
+const bar = document.querySelector(".bar");
+
+window.addEventListener("scroll", () => {
+    const totalHeight = window.innerHeight;
+    const fullHeight = document.documentElement.scrollHeight;
+    const scrolled = window.scrollY;
+    const progress = (scrolled / (fullHeight - totalHeight)) * 100;
+    bar.style.width = `${progress}%`;
 })
